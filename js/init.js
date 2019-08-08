f905209e273dd606ddfb051ebe4dd67776446108
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2019-08-08 23:21:53 +0200

    fix bug in small devices navigator + sort pages in header

--- js/init.js
+++ js/init.js
@@ -1,11 +1,7 @@
-// (function($){
-//   $(function(){
-//
-//     $('.button-collapse').sideNav();
-//     $('.carousel').carousel();
-//
-//   }); // end of document ready
-// })(jQuery); // end of jQuery name space
+$(document).ready(function(){
+  console.log('cio');
+  $('.side-nav').sidenav();
+});
 
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
