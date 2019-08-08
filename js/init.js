69226e6e43cf63d18cdcf505b421b2c62c81e5ca
Gabriele Graffieti <gabriele.graffieti@unibo.it>
2019-08-08 13:08:08 +0200

    add particles-js

--- js/init.js
+++ js/init.js
@@ -1,8 +1,12 @@
-(function($){
-  $(function(){
+// (function($){
+//   $(function(){
+//
+//     $('.button-collapse').sideNav();
+//     $('.carousel').carousel();
+//
+//   }); // end of document ready
+// })(jQuery); // end of jQuery name space
 
-    $('.button-collapse').sideNav();
-    $('.carousel').carousel();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
